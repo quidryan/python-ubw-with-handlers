881fc1e7b5d7a8c212027366a058485c20edf01d
lofiwacker <lofiwacker@a42fbec5-434b-0410-9dff-3d12a510563e>
2008-04-20 01:37:25 +0000

    Adding skeleton

---
+++ setup.py
@@ -0,0 +1,13 @@
+# setup.py
+from distutils.core import setup
+
+setup(name="ubw",
+      description="USB Bit Wacker Interface",
+      version="0.1",
+      author="Lofi Wacker",
+      author_email="lofiwacker@gmail.com",
+      url="http://code.google.com/p/python-ubw/",
+      packages=["ubw"],
+      license="GPLv2",
+      long_description="USB Bit Wacker Interface",
+)
